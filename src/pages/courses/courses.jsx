5a88ea05d4f8663e1da9f8b98471e7a972029620
James Yaro <onuhjamesyaro@gmail.com>
2022-10-02 18:36:06 +0100

    Create the Course page
    The course page contains:
    - Header link and buttons
    - Student list
    - List of topic
    - Video area

---
+++ src/pages/courses/courses.jsx
@@ -0,0 +1,104 @@
+import React, {useState} from 'react'
+import './courses.css'
+import { Link } from 'react-router-dom'
+
+function Courses() {
+
+    const [topic, setTopic] = useState(true)
+
+    const handleStudent = () => {
+        setTopic(false)
+    }
+    const handleTopic = () => {
+        setTopic(true)
+    }
+
+
+    const courses = [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur"
+    ]
+
+    const students = [
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+        "Lorem ipsum",
+    ]
+    return (
+        <div className='courses-container'>
+            <div className="courses-header">
+                <Link className='courses-link'><span><i className='fa fa-angle-left'></i></span><p>Your courses</p></Link>
+            </div>
+            <div className="courses-layout">
+                <div className="courses-left">
+                    <div className="courses-left-buttons">
+                        <button className={topic ? 'courses-left-button active' : "courses-left-button"} onClick={handleTopic}>Topics</button>
+                        <button className={topic ? 'courses-left-button' : "courses-left-button active"} onClick={handleStudent}>Students</button>
+                    </div>
+                    <div className="courses-left-content">
+                        {topic ? <div className="course-content">
+                            {courses.map((course, index) => (
+                                <div className='i-content'>
+                                    <span><i className="i-icon fa fa-play"></i></span>
+                                    <p key={index} className="i-text">{course}</p>
+                                </div>
+                            ))}
+                        </div> :  <div className='student-lists'>
+                            {students.map((student, index) => (
+                            <div key ={index} className="i-student">
+                                <div className="i-student-name">
+                                    <img className='i-student-img' src='/assets/profile.jpg' />
+                                    <p>{student}</p>
+                                </div>
+                                <button className='i-student-button'>Write a message</button>
+                            </div>
+                            ))}
+                        </div> }
+                        
+                       
+                    </div>
+                </div>
+                <div className="courses-right">
+                    <div className="courses-right-buttons">
+                        <button className="courses-right-button">Video</button>
+                        <button className="courses-right-button">Lab</button>
+                        <button className="courses-right-button">Note</button>
+                        <button className="courses-right-button">Chat</button>
+                    </div>
+                    <div className="courses-right-content">
+                        <div className='vid-box'>
+                            <img src='/assets/image2.png' alt="Video playing..." className="vid" />
+                        </div>
+                        <p className='vid-title'>Python as a Computer Programming Language</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Courses
