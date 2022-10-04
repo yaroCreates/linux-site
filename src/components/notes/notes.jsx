ca34ef1018d33903fbdb8b6dafeeb077d54b1178
James Yaro <onuhjamesyaro@gmail.com>
2022-10-04 23:25:46 +0100

    create note object and rendered it

--- src/components/notes/notes.jsx
+++ src/components/notes/notes.jsx
@@ -1,26 +1,74 @@
 import React from 'react'
 import './notes.css'
 
+
+const content = [
+
+    {
+        "title": 'Python as a Programming Language',
+        "summary": 'In this Lab we will be discussing an alternative\
+                    to using your PC and IDE (Integrated Development environment )\
+                    for running and writing your python programs, interesting right?\
+                    Yeah. This alternative is using your mobile phone to run your codes.\
+                    There are several application available for doing this, but for the\
+                    sake of this lab, we will be focusing on Qpython3. Feel free to research\
+                    and use other. 1. Download the Application\
+                    Qpython3 is available on both the Google Playstore and Apple Appstore'
+    },
+    {
+        "title": 'Python as a Programming Language',
+        "summary": 'In this Lab we will be discussing an alternative\
+                    to using your PC and IDE (Integrated Development environment )\
+                    for running and writing your python programs, interesting right?\
+                    Yeah. This alternative is using your mobile phone to run your codes.\
+                    There are several application available for doing this, but for the\
+                    sake of this lab, we will be focusing on Qpython3. Feel free to research\
+                    and use other. 1. Download the Application\
+                    Qpython3 is available on both the Google Playstore and Apple Appstore'
+    },
+    {
+        "title": 'Python as a Programming Language',
+        "summary": 'In this Lab we will be discussing an alternative\
+                    to using your PC and IDE (Integrated Development environment )\
+                    for running and writing your python programs, interesting right?\
+                    Yeah. This alternative is using your mobile phone to run your codes.\
+                    There are several application available for doing this, but for the\
+                    sake of this lab, we will be focusing on Qpython3. Feel free to research\
+                    and use other. 1. Download the Application\
+                    Qpython3 is available on both the Google Playstore and Apple Appstore'
+    },
+    {
+        "title": 'Python as a Programming Language',
+        "summary": 'In this Lab we will be discussing an alternative\
+                    to using your PC and IDE (Integrated Development environment )\
+                    for running and writing your python programs, interesting right?\
+                    Yeah. This alternative is using your mobile phone to run your codes.\
+                    There are several application available for doing this, but for the\
+                    sake of this lab, we will be focusing on Qpython3. Feel free to research\
+                    and use other. 1. Download the Application\
+                    Qpython3 is available on both the Google Playstore and Apple Appstore'
+    },
+]
 function Notes() {
     return (
         <div className='note-container'>
-            <div className="note">
-                <div className='note-header'>
-                    <div className='icon-title'>
-                        <span><i className='fa fa-play'></i></span>
-                        <p className='note-title'>Python as a Computer Programming Language</p>
+            {content.map((item, index) => (
+                <div key={index} className="note">
+                    <div className='note-header'>
+                        <div className='icon-title'>
+                            <span><i className='fa fa-play'></i></span>
+                            <p className='note-title'>{item.title}</p>
+                        </div>
+                        <p className="note-header-link">Go to the video <span><i className="fa fa-angle-right"></i></span></p>
                     </div>
-                    <p className="note-header-link">Go to the video <span><i className="fa fa-angle-right"></i></span></p>
-                </div>
-                <div className="note-body">
-                    <p className="body-text">
-                        In this Lab we will be discussing an alternative to using your PC and IDE (Integrated Development environment ) for running and writing your python programs, interesting right? Yeah. This alternative is using your mobile phone to run your codes. There are several application available for doing this, but for the sake of this lab, we will be focusing on Qpython3. Feel free to research and use other. 1. Download the Application
-                        Qpython3 is available on both the Google Playstore and Apple Appstore.
-                    </p>
+                    <div className="note-body">
+                        <p className="body-text">
+                            {item.summary}
+                        </p>
+                    </div>
                 </div>
 
-
-            </div>
+            ))}
         </div>
     )
 }
