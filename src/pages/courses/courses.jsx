6f154a465b13176b3595cce5bc99cd8325973601
James Yaro <onuhjamesyaro@gmail.com>
2022-10-05 23:15:53 +0100

    change link to Navlink

--- src/pages/courses/courses.jsx
+++ src/pages/courses/courses.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './courses.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, NavLink } from 'react-router-dom'
 import Video from '../../components/video/video'
 import Notes from '../../components/notes/notes'
 
@@ -69,32 +69,32 @@ function Courses() {
                                     <p key={index} className="i-text">{course}</p>
                                 </div>
                             ))}
-                        </div> :  <div className='student-lists'>
+                        </div> : <div className='student-lists'>
                             {students.map((student, index) => (
-                            <div key ={index} className="i-student">
-                                <div className="i-student-name">
-                                    <img className='i-student-img' src='/assets/profile.jpg' />
-                                    <p>{student}</p>
+                                <div key={index} className="i-student">
+                                    <div className="i-student-name">
+                                        <img className='i-student-img' src='/assets/profile.jpg' />
+                                        <p>{student}</p>
+                                    </div>
+                                    <button className='i-student-button'>Write a message</button>
                                 </div>
-                                <button className='i-student-button'>Write a message</button>
-                            </div>
                             ))}
-                        </div> }
-                        
-                       
+                        </div>}
+
+
                     </div>
                 </div>
                 <div className="courses-right">
                     <div className="courses-right-buttons">
-                        <Link to='/courses'><button className="courses-right-button">Video</button></Link>
-                        <Link><button className="courses-right-button">Lab</button></Link>
-                        <Link to='/courses/notes'><button className="courses-right-button">Note</button></Link>
-                        <Link><button className="courses-right-button">Chat</button></Link>
+                        <NavLink to='/courses'><button className="courses-right-button">Video</button></NavLink>
+                        <NavLink><button className="courses-right-button">Lab</button></NavLink>
+                        <NavLink to='/courses/notes'><button className="courses-right-button">Note</button></NavLink>
+                        <NavLink><button className="courses-right-button">Chat</button></NavLink>
                     </div>
                     <div className="courses-right-content">
                         <Routes>
-                            <Route exact path='/' element={<Video/>}/>
-                            <Route exact path='notes' element={<Notes/>}/>
+                            <Route exact path='/' element={<Video />} />
+                            <Route exact path='notes' element={<Notes />} />
                         </Routes>
                     </div>
                 </div>
